cd045a85ecc4926a12e4db416a71414646fd5758
james@jamespamplin.com <james@jamespamplin.com>
2015-08-11 15:26:04 +0100

    Init viewer iframe of preview content

---
+++ app/com/gu/viewer/config/Configuration.scala
@@ -0,0 +1,10 @@
+package com.gu.viewer.config
+
+
+object Configuration {
+
+  val previewHost = "preview.gutools.co.uk"
+  val liveHost = "www.theguardian.com"
+
+  val stage = "PROD"
+}
--- app/com/gu/viewer/controllers/Application.scala
+++ app/com/gu/viewer/controllers/Application.scala
@@ -2,12 +2,23 @@ package com.gu.viewer.controllers
 
 import play.api._
 import play.api.mvc._
+import com.gu.viewer.config.Configuration
 import com.gu.viewer.views.html
 
 class Application extends Controller {
 
   def index = Action {
     Ok(html.index("Your new application is ready."))
+  }
+
+  def viewer(target: String, path: String) = Action {
+    val viewerDomain = target match {
+      case "preview" => Configuration.previewHost
+      case _ => Configuration.liveHost
+    }
+    val viewerUrl = s"https://$viewerDomain/$path"
+
+    Ok(html.viewer(viewerUrl))
   }
 
 }
