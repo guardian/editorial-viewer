6ac14b8eeb0719b8c1175049cc46b466f146764f
Christopher Lloyd <christopher.loyd@guardian.co.uk>
2015-08-14 14:39:33 +0100

    Added some fancy animations

--- public/javascript/components/viewer.js
+++ public/javascript/components/viewer.js
@@ -1,18 +1,45 @@
 
 var viewerEl = document.getElementById('viewer');
 
+var currentViewPortConfig;
 var currentViewPortName;
 
-function applyViewerStyle(viewportName, viewportConfig) {
+function updateViewer(viewportName, viewportConfig) {
 
-    viewerEl.className = 'viewer is-' + viewportName;
+    var isAnimated = false;
 
-    viewerEl.style.width = viewportConfig.width;
-    viewerEl.style.height = viewportConfig.height;
+    if (currentViewPortConfig && currentViewPortConfig !== viewportConfig) {
+        //We have a change of viewport, test for special cases where we can animate
+        if (viewportConfig.isMobile && currentViewPortConfig.isMobile) {
+            isAnimated = true;
+        }
+    }
 
+    currentViewPortConfig = viewportConfig;
     currentViewPortName = viewportName;
+
+    restyleViewer(isAnimated);
+}
+
+function restyleViewer(isAnimated) {
+
+    var transitionEndHandler = function() {
+        viewerEl.removeEventListener('transitionend', transitionEndHandler);
+        viewerEl.classList.remove('is-animated');
+    };
+
+    viewerEl.className = 'viewer is-' + currentViewPortName;
+
+    if (isAnimated) {
+        viewerEl.classList.add('is-animated');
+        viewerEl.addEventListener('transitionend', transitionEndHandler);
+    }
+
+    viewerEl.style.width = currentViewPortConfig.width;
+    viewerEl.style.height = currentViewPortConfig.height;
+
 }
 
 module.exports = {
-    updateViewer: applyViewerStyle
+    updateViewer: updateViewer
 };
