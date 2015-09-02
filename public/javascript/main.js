4d2757d71c0c240eb4e661a2b4bafb023b74cc69
Christopher Lloyd <christopher.lloyd@theguardian.co.uk>
2015-09-02 17:06:25 +0100

    Basic history sync

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,9 +1,11 @@
 var applicationCtrl = require('javascript/controllers/application');
 var analyticsCtrl = require('javascript/controllers/analytics');
+var historyCtrl = require('javascript/controllers/history');
 
 //Initialize Controllers
 applicationCtrl.init();
 analyticsCtrl.init();
+historyCtrl.init();
 
 //Track Inital View
 analyticsCtrl.recordPageOpen();
