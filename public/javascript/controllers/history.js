a1a213994394db9918aa18e9c09bd378edc1a1aa
Christopher Lloyd <christopher.lloyd@theguardian.co.uk>
2015-09-03 16:29:32 +0100

    Improved back naivagation detection

--- public/javascript/controllers/history.js
+++ public/javascript/controllers/history.js
@@ -1,28 +1,40 @@
 var viewer = require('../components/viewer');
+var currentPath;
 
 function init() {
-    //Bind to history change (for back buttons!)
     window.addEventListener('popstate', onPopState);
 
-    //Bind to viewer load event (Probably want to do this through the viewer component)
     document.getElementById('viewer').addEventListener('load', function(e){
-        //Validate it is an actual page (not blank, chrome error)
         var iframeLocation = e.target.contentWindow.location;
 
         if (iframeLocation.origin !== "null" || iframeLocation.protocol.indexOf('http') !== -1) {
-            addLocationHistory(iframeLocation.pathname);
+            addLocationHistory(iframeLocation);
         }
     })
 }
 
 function onPopState(e){
     //TODO REMOVE HARDCODED PREVIEW
-    viewer.updateUrl('http://preview.gutools.co.uk' + e.state.path)
+    
+    currentPath = e.state.viewerHref;
+    viewer.updateUrl(e.state.viewerHref)
 }
 
-function addLocationHistory(path) {
-    //TODO REMOVE HARDCODED VIEWER URL
-    window.history.pushState({path: path}, "", "/preview" + path);
+function addLocationHistory(location) {
+
+    var path = location.pathname;
+    var href = location.href;
+
+    //Check not already there, or not first load.
+
+    if (!currentPath || currentPath === href) {
+        currentPath = href;
+        return;
+    }
+
+    //TODO REMOVE HARDCODED PREVIEW URL
+    window.history.pushState({viewerHref: href}, "", '/preview' + path);
+    currentPath = href;
 }
 
 module.exports = {
