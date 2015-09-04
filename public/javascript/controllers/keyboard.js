7c2becd8484f0a26855ab2030032a9564817302c
Christopher Lloyd <christopher.lloyd@theguardian.co.uk>
2015-09-04 16:00:59 +0100

    Added experimental reader mode

--- public/javascript/controllers/keyboard.js
+++ public/javascript/controllers/keyboard.js
@@ -1,19 +1,27 @@
 var viewer = require('../components/viewer');
+var applicationController = require('./application');
 var alreadyRan = false;
 
 
 
 function onKeyPress(e) {
+
     if ((e.ctrlKey || e.metaKey) && e.keyIdentifier === "U+0050") {
       viewer.printViewer();
+      e.preventDefault();
+    }
+
+    if ((e.ctrlKey || e.metaKey) && e.keyIdentifier === "U+0042") {
+      applicationController.setMode('reader');
       e.preventDefault();
     }
 }
 
 
+
 function init() {
     if (alreadyRan) {
-        console.log("Already init'd keyboardController")
+        console.log("Already init'd keyboardController");
         return;
     }
 
