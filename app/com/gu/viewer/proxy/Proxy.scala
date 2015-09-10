50b5b5118bf5683b45a2be1214f304252a3f9456
james@jamespamplin.com <james@jamespamplin.com>
2015-09-10 12:10:15 +0100

    Simplified proxy client syntax

--- app/com/gu/viewer/proxy/Proxy.scala
+++ app/com/gu/viewer/proxy/Proxy.scala
@@ -5,51 +5,65 @@ import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.ws.{WSResponse, WSClient}
 import play.api.mvc.{Cookies, Cookie, Result}
 import play.api.mvc.Results.Status
-import play.api.http.HeaderNames.COOKIE
+import play.api.http.HeaderNames.{CONTENT_LENGTH, CONTENT_TYPE, COOKIE}
+import play.api.http.MimeTypes.TEXT
 
 import scala.concurrent.Future
 
 @Singleton
-class Proxy @Inject() (ws: WSClient) {
+class Proxy @Inject()(ws: WSClient) {
 
   private val TIMEOUT = 10000
 
-  case class ProxyRequest(
-                           destination: String,
-                           headers: Seq[(String, String)] = Seq.empty,
-                           cookies: Seq[Cookie] = Seq.empty,
-                           queryString: Seq[(String, String)] = Seq.empty,
-                           followRedirects: Boolean = false
-                         ) {
+  private def proxy(
+             method: String,
+             destination: String,
+             headers: Seq[(String, String)] = Seq.empty,
+             cookies: Seq[Cookie] = Seq.empty,
+             queryString: Seq[(String, String)] = Seq.empty,
+             body: Map[String, Seq[String]] = Map.empty,
+             followRedirects: Boolean = false
+             )(handler: PartialFunction[WSResponse, Future[Result]] = PartialFunction.empty): Future[Result] = {
 
     val cookieHeader = if (cookies.nonEmpty) Some(COOKIE -> Cookies.encodeCookieHeader(cookies)) else None
 
-    val wsRequest = ws.url(destination)
-      .withFollowRedirects(follow = followRedirects)
-      .withHeaders(headers ++ cookieHeader: _*)
-      .withQueryString(queryString: _*)
-      .withRequestTimeout(TIMEOUT)
+    val contentLengthHeader = if (body.nonEmpty) Some(CONTENT_LENGTH -> body.size.toString) else None
 
-    def get(handler: PartialFunction[WSResponse, Future[Result]] = PartialFunction.empty) = {
-      wsRequest
-        .get()
-        .flatMap(handler orElse handleResponse)
-    }
-
-    def post(data: Map[String, Seq[String]] = Map.empty)(handler: PartialFunction[WSResponse, Future[Result]] = PartialFunction.empty) = {
-      wsRequest
-        .withHeaders("Content-Length" -> "0")
-        .post(data)
-        .flatMap(handler orElse handleResponse)
-    }
-
-    private def handleResponse: PartialFunction[WSResponse, Future[Result]] = {
+    def handleResponse: PartialFunction[WSResponse, Future[Result]] = {
       case (response) => Future.successful {
         Status(response.status)(response.body)
-          .as(response.header("Content-Type").getOrElse("text/plain"))
+          .as(response.header(CONTENT_TYPE).getOrElse(TEXT))
       }
     }
 
+    ws.url(destination)
+      .withFollowRedirects(follow = followRedirects)
+      .withHeaders(headers ++ contentLengthHeader ++ cookieHeader: _*)
+      .withQueryString(queryString: _*)
+      .withRequestTimeout(TIMEOUT)
+      .withBody(body)
+      .execute(method).flatMap(handler orElse handleResponse)
   }
+
+
+  def get(
+           destination: String,
+           headers: Seq[(String, String)] = Seq.empty,
+           cookies: Seq[Cookie] = Seq.empty,
+           queryString: Seq[(String, String)] = Seq.empty,
+           followRedirects: Boolean = false
+           )(handler: PartialFunction[WSResponse, Future[Result]] = PartialFunction.empty) =
+    proxy("GET", destination, headers, cookies, queryString, followRedirects = followRedirects)(handler)
+
+
+  def post(
+            destination: String,
+            headers: Seq[(String, String)] = Seq.empty,
+            cookies: Seq[Cookie] = Seq.empty,
+            queryString: Seq[(String, String)] = Seq.empty,
+            body: Map[String, Seq[String]] = Map.empty,
+            followRedirects: Boolean = false
+            )(handler: PartialFunction[WSResponse, Future[Result]] = PartialFunction.empty) =
+    proxy("POST", destination, headers, cookies, queryString, body, followRedirects)(handler)
 
 }
