75706796cce290947263efddfd86608b607e1305
james@jamespamplin.com <james@jamespamplin.com>
2015-09-14 14:34:49 +0100

    Extract result handling to ProxyResult. Properly fail futures with ProxyError

--- app/com/gu/viewer/proxy/PreviewProxy.scala
+++ app/com/gu/viewer/proxy/PreviewProxy.scala
@@ -4,10 +4,9 @@ import javax.inject.{Inject, Singleton}
 import com.gu.viewer.config.Configuration
 import com.gu.viewer.controllers.routes
 import com.gu.viewer.logging.Loggable
-import com.gu.viewer.views.html
-import play.api.mvc.Results.{Ok, BadGateway, BadRequest, Redirect}
-
+import play.api.mvc.Result
 import scala.concurrent.Future
+
 
 @Singleton
 class PreviewProxy @Inject() (proxyClient: Proxy) extends Loggable {
@@ -16,14 +15,14 @@ class PreviewProxy @Inject() (proxyClient: Proxy) extends Loggable {
   val previewLoginUrl = s"http://$serviceHost/login"
 
 
-  def loginCallbackUrl(request: PreviewProxyRequest) =
+  private def loginCallbackUrl(request: PreviewProxyRequest) =
     s"${request.protocol}://${request.requestHost}${routes.Proxy.previewAuthCallback()}"
 
 
   /**
    * Transform proxy server relative URI to viewer URI.
    */
-  def proxyUriToViewerUri(uri: String) = {
+  private def proxyUriToViewerUri(uri: String) = {
     val proxyUri = """^\/proxy(\/.+)$""".r
 
     uri match {
@@ -32,7 +31,7 @@ class PreviewProxy @Inject() (proxyClient: Proxy) extends Loggable {
     }
   }
 
-  def doPreviewAuth(request: PreviewProxyRequest) = {
+  private def doPreviewAuth(request: PreviewProxyRequest) = {
     val proxyRequestUrl = previewLoginUrl
 
     log.info(s"Proxy Preview auth to: $proxyRequestUrl")
@@ -47,25 +46,26 @@ class PreviewProxy @Inject() (proxyClient: Proxy) extends Loggable {
         .withPlaySessionFrom(request.session)
         .withReturnUrl(Some(returnUrl))
 
-      val loc = response.header("Location").get
+      val locHeader = response.header("Location")
 
-      session.sessionCookie match {
-        case Some(_) =>
-          Ok(html.loginRedirect(loc))
-            .withSession(session.asPlaySession)
+      (session.sessionCookie, locHeader) match {
+        case (Some(_), Some(location)) => Future.successful {
+          PreviewAuthRedirectProxyResult(location, session)
+        }
 
-        case None => badGatewayResponse("Unexpected response from preview login request", response)
+        case (None, _) => error("Unexpected response from preview login request", response)
+        case (_, None) => error("Invalid response from preview login request", response)
       }
     }
 
     proxyClient.post(proxyRequestUrl, queryString = Seq("redirect-url" -> loginCallbackUrl(request))) {
-      case response if response.status == 303 => Future.successful(handleResponse(response))
+      case response if response.status == 303 => handleResponse(response)
       // TODO should we handle non redirect responses here?
     }
   }
 
 
-  def doPreviewProxy(request: PreviewProxyRequest) = {
+  private def doPreviewProxy(request: PreviewProxyRequest) = {
 
     val url = s"${request.protocol}://$serviceHost/${request.servicePath}"
     log.info(s"Proxy to preview: $url")
@@ -91,13 +91,15 @@ class PreviewProxy @Inject() (proxyClient: Proxy) extends Loggable {
   }
 
 
-  def proxy(request: PreviewProxyRequest) = {
-      request.session.sessionCookie -> request.session.authCookie match {
-        case (Some(_), Some(_)) => doPreviewProxy(request)
-        case _ => doPreviewAuth(request)
-      }
+  /**
+   * Entry-point for proxying a request to preview
+   */
+  def proxy(request: PreviewProxyRequest): Future[Result] = ProxyResult.resultFrom {
+    request.session.sessionCookie -> request.session.authCookie match {
+      case (Some(_), Some(_)) => doPreviewProxy(request)
+      case _ => doPreviewAuth(request)
+    }
   }
-
 
 
   /**
@@ -106,7 +108,7 @@ class PreviewProxy @Inject() (proxyClient: Proxy) extends Loggable {
    * Proxy all request params and Preview session cookie to Preview authentication callback.
    * Store response cookies into Viewer's play session.
    */
-  def previewAuthCallback(request: PreviewProxyRequest) = {
+  def previewAuthCallback(request: PreviewProxyRequest) = ProxyResult.resultFrom {
 
     val redirectUrlParam = "redirect-url" -> loginCallbackUrl(request)
     val queryParams = request.requestQueryString.mapValues(_.head).toSeq :+ redirectUrlParam
@@ -117,19 +119,18 @@ class PreviewProxy @Inject() (proxyClient: Proxy) extends Loggable {
         .withPlaySessionFrom(request.session)
 
       (session.sessionCookie, session.authCookie) match {
-        case (Some(_), Some(_)) => {
+        case (Some(_), Some(_)) => Future.successful {
           val returnUrl = request.session.returnUrl.getOrElse("/proxy/preview/uk")
-          Redirect(returnUrl)
-            .withSession(session.withoutReturnUrl.asPlaySession)
+          RedirectProxyResultWithSession(returnUrl, session.withoutReturnUrl)
         }
-        case (None, None) => badGatewayResponse("Bad response from preview auth callback", response)
-        case (None, _) => badGatewayResponse("Preview Session cookie not returned", response)
-        case (_, None) => badGatewayResponse("Preview Auth cookie not returned", response)
+        case (None, None) => error("Bad response from preview auth callback", response)
+        case (None, _) => error("Preview Session cookie not returned", response)
+        case (_, None) => error("Preview Auth cookie not returned", response)
       }
     }
 
     request.session.sessionCookie match {
-      case None => Future.successful(BadRequest("Preview session not established"))
+      case None => error("Preview session not established")
 
       case Some(_) => {
         val proxyUrl = s"http://${Configuration.previewHost}/oauth2callback"
@@ -138,19 +139,17 @@ class PreviewProxy @Inject() (proxyClient: Proxy) extends Loggable {
         val cookies = request.session.asCookies
 
         proxyClient.get(proxyUrl, queryString = queryParams, cookies = cookies) {
-          case r => Future.successful(handleResponse(r))
+          case r => handleResponse(r)
         }
       }
     }
   }
 
+  private def error(msg: String) =
+    Future.failed(ProxyError(msg, None))
 
-
-  private def badGatewayResponse(msg: String, response: ProxyResponse) = {
-    log.warn(s"$msg: ${response.toString} ${response.allHeaders} ${response.body}")
-    BadGateway(msg)
-  }
-
+  private def error(msg: String, response: ProxyResponse) =
+    Future.failed(ProxyError(msg, Some(response)))
 
 }
 
--- app/com/gu/viewer/proxy/Proxy.scala
+++ app/com/gu/viewer/proxy/Proxy.scala
@@ -3,11 +3,8 @@ package com.gu.viewer.proxy
 import javax.inject.{Inject, Singleton}
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.ws.WSClient
-import play.api.mvc.{Cookies, Cookie, Result}
-import play.api.mvc.Results.Status
-import play.api.http.HeaderNames.{CONTENT_LENGTH, CONTENT_TYPE, COOKIE}
-import play.api.http.MimeTypes.TEXT
-
+import play.api.mvc.{Cookies, Cookie}
+import play.api.http.HeaderNames.{CONTENT_LENGTH, COOKIE}
 import scala.concurrent.Future
 
 @Singleton
@@ -23,23 +20,15 @@ class Proxy @Inject()(ws: WSClient) {
              queryString: Seq[(String, String)] = Seq.empty,
              body: Map[String, Seq[String]] = Map.empty,
              followRedirects: Boolean = false
-             )(handler: PartialFunction[ProxyResponse, Future[Result]] = PartialFunction.empty): Future[Result] = {
+             )(handler: PartialFunction[ProxyResponse, Future[ProxyResult]] = PartialFunction.empty): Future[ProxyResult] = {
 
     val cookieHeader = if (cookies.nonEmpty) Some(COOKIE -> Cookies.encodeCookieHeader(cookies)) else None
 
     val contentLengthHeader = if (body.nonEmpty) Some(CONTENT_LENGTH -> body.size.toString) else None
 
-    def handleResponse: PartialFunction[ProxyResponse, Future[Result]] = {
-      case response => Future.successful {
-        val resultHeaders = Seq(
-          response.header(CONTENT_LENGTH).map(CONTENT_LENGTH -> _)
-        ).flatten
-
-        Status(response.status)
-          .stream(response.body)
-          .withHeaders(resultHeaders: _*)
-          .as(response.header(CONTENT_TYPE).getOrElse(TEXT))
-      }
+    def handleResponse: PartialFunction[ProxyResponse, Future[ProxyResult]] = {
+      case response =>
+        Future.successful(ProxyResultWithBody(response))
     }
 
     ws.url(destination)
@@ -61,7 +50,7 @@ class Proxy @Inject()(ws: WSClient) {
            cookies: Seq[Cookie] = Seq.empty,
            queryString: Seq[(String, String)] = Seq.empty,
            followRedirects: Boolean = false
-           )(handler: PartialFunction[ProxyResponse, Future[Result]] = PartialFunction.empty) =
+           )(handler: PartialFunction[ProxyResponse, Future[ProxyResult]] = PartialFunction.empty) =
     proxy("GET", destination, headers, cookies, queryString, followRedirects = followRedirects)(handler)
 
 
@@ -72,7 +61,7 @@ class Proxy @Inject()(ws: WSClient) {
             queryString: Seq[(String, String)] = Seq.empty,
             body: Map[String, Seq[String]] = Map.empty,
             followRedirects: Boolean = false
-            )(handler: PartialFunction[ProxyResponse, Future[Result]] = PartialFunction.empty) =
+            )(handler: PartialFunction[ProxyResponse, Future[ProxyResult]] = PartialFunction.empty) =
     proxy("POST", destination, headers, cookies, queryString, body, followRedirects)(handler)
 
 }
