f740185c98f201fb0404e995acf172974829cd23
Sam Cutler <sam92cutler@hotmail.co.uk>
2016-03-08 16:05:04 +0000

    Revert "App preview button and emailing"

--- app/com/gu/viewer/aws/AWS.scala
+++ app/com/gu/viewer/aws/AWS.scala
@@ -3,7 +3,6 @@ package com.gu.viewer.aws
 import com.amazonaws.regions.{Regions, Region}
 import com.amazonaws.services.ec2.AmazonEC2Client
 import com.amazonaws.services.ec2.model.{Filter, DescribeTagsRequest}
-import com.amazonaws.services.simpleemail._
 import com.amazonaws.util.EC2MetadataUtils
 import scala.collection.JavaConverters._
 
@@ -14,9 +13,6 @@ object AWS {
   lazy val EC2Client = region.createClient(classOf[AmazonEC2Client], null, null)
 
   lazy val instanceId = Option(EC2MetadataUtils.getInstanceId)
-
-  lazy val emailClient = new AmazonSimpleEmailServiceClient()
-  emailClient.setRegion(region)
 
   def readTag(tagName: String) = {
     instanceId.flatMap { id =>
