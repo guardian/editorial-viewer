aaf97649562e365bd9ec6a7f99c42a25fe7be106
jharewinton <jonathan.hare-winton@guardian.co.uk>
2016-12-13 16:54:13 +0000

    add HttpsRedirectFilter to redirect http traffic to https

--- app/com/gu/viewer/Filters.scala
+++ app/com/gu/viewer/Filters.scala
@@ -4,10 +4,26 @@ import javax.inject.Inject
 
 import com.gu.viewer.logging.RequestLoggingFilter
 import play.api.http.HttpFilters
+import play.api.mvc.{Filter, RequestHeader, Result, Results}
+
+import scala.concurrent.Future
 
 
 class Filters @Inject() (logger: RequestLoggingFilter) extends HttpFilters {
 
   val filters = Seq(logger)
 
+}
+
+object HttpsRedirectFilter extends Filter {
+
+  def apply(nextFilter: (RequestHeader) => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
+    //play uses lower case headers.
+    requestHeader.headers.get("x-forwarded-proto") match {
+      case Some("http") => {
+          Future.successful(Results.Redirect("https://" + requestHeader.host + requestHeader.uri, 301))
+      }
+      case _ => nextFilter(requestHeader)
+    }
+  }
 }
