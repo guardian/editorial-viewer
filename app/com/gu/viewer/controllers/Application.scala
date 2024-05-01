0b5149223184c918d277618f8f72f2d8a582dff9
Tom Richards <tom.richards@guardian.co.uk>
2024-05-01 12:20:03 +0100

    use panda auth for all user facing endpoints (so the panda cookie is present before calling preview)

--- app/com/gu/viewer/controllers/Application.scala
+++ app/com/gu/viewer/controllers/Application.scala
@@ -1,15 +1,26 @@
 package com.gu.viewer.controllers
 
+import com.gu.pandomainauth.PanDomainAuthSettingsRefresher
 import com.gu.viewer.config.AppConfig
 import com.gu.viewer.logging.Loggable
 import com.gu.viewer.views.html
+import play.api.libs.ws.WSClient
 import play.api.mvc._
 import play.filters.csrf.CSRF
 
-class Application(val controllerComponents: ControllerComponents, config: AppConfig)
-  extends BaseController with Loggable {
+class Application(
+  val controllerComponents: ControllerComponents,
+  val wsClient: WSClient,
+  val config: AppConfig,
+  val panDomainSettings: PanDomainAuthSettingsRefresher
+) extends BaseController with Loggable with PanDomainAuthActions {
 
-  def index = Action {
+  def oauthCallback: Action[AnyContent] =
+    Action.async { implicit request =>
+      processOAuthCallback()
+    }
+
+  def index = AuthAction {
     Redirect("/live/uk")
   }
 
@@ -21,7 +32,7 @@ class Application(val controllerComponents: ControllerComponents, config: AppCon
     viewer("live", path, "live")
   }
 
-  def viewer(target: String, path: String, previewEnv: String) = Action { implicit request =>
+  def viewer(target: String, path: String, previewEnv: String) = AuthAction { implicit request =>
     val protocol = if (request.secure) "https" else "http"
     val viewerHost = target match {
       case "preview" => config.previewHost
