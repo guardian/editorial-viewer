131d29db389b086ff079bf207c71b256f14f308a
David Blatcher <dv.bltchr@gmail.com>
2024-07-09 15:43:30 +0100

    add some empty test suites

---
+++ test/scala/com/gu/viewer/config/AppConfigTest.scala
@@ -0,0 +1,12 @@
+package com.gu.viewer.config
+
+import org.scalatest.featurespec.AnyFeatureSpec
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers
+
+class AppConfigTest extends AnyFunSuite with Matchers {
+  
+
+
+}
+
---
+++ test/scala/com/gu/viewer/controllers/EmailTest.scala
@@ -0,0 +1,9 @@
+package com.gu.viewer.controllers
+
+import org.scalatest.featurespec.AnyFeatureSpec
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers
+
+class EmailTest extends AnyFunSuite with Matchers {
+
+}
