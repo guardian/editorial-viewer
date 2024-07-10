3eb83def1654f997493a41704191a73f4b694220
David Blatcher <dv.bltchr@gmail.com>
2024-07-10 14:33:48 +0100

    scaffold test suites

---
+++ test/com/gu/viewer/config/AppConfigTest.scala
@@ -0,0 +1,30 @@
+package com.gu.viewer.config
+
+import com.gu.viewer.aws.AwsInstanceTags
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers
+import org.mockito.MockitoSugar
+import play.api.Configuration
+
+class AppConfigTest extends AnyFunSuite with Matchers with MockitoSugar {
+
+  val devConfigValues: Map[String, Any] = Map.apply(
+    "Stage" -> "DEV",
+    "previewHost.DEV" -> "bar",
+    "liveHost.DEV" -> "bar",
+    "composerReturnUri.DEV" -> "bar"
+  )
+
+  var mockTags: AwsInstanceTags = mock[AwsInstanceTags]
+  var devConfig: Configuration = Configuration.from(devConfigValues)
+
+  test ("can construct with DEV stage") {
+    when(mockTags.readTag("Stage")).thenReturn(None)
+    when(mockTags.readTag("App")).thenReturn(None)
+    val instance: AppConfig = new AppConfig(mockTags,devConfig)
+    instance.app must be ("viewer")
+    instance.pandaDomain must be ("local.dev-gutools.co.uk")
+    instance.pandaAuthCallback must be ("https://viewer.local.dev-gutools.co.uk/oauthCallback")
+  }
+}
+
---
+++ test/com/gu/viewer/controllers/PanDomainAuthActionsTest.scala
@@ -0,0 +1,38 @@
+package com.gu.viewer.controllers
+
+import com.gu.viewer.aws.AwsInstanceTags
+import com.gu.viewer.config.AppConfig
+import com.gu.pandomainauth.PanDomainAuthSettingsRefresher
+
+import org.scalatest.funsuite.AnyFunSuite;
+import org.mockito.MockitoSugar;
+import play.api.Configuration;
+import play.api.libs.ws.WSClient;
+import play.api.mvc.ControllerComponents;
+import org.scalatest.matchers.must.Matchers;
+
+class PanDomainAuthActionsTest extends AnyFunSuite with Matchers with MockitoSugar {
+
+  val devConfigValues: Map[String, Any] = Map.apply(
+    "Stage" -> "DEV",
+    "previewHost.DEV" -> "bar",
+    "liveHost.DEV" -> "bar",
+    "composerReturnUri.DEV" -> "bar"
+  )
+
+  var mockTags: AwsInstanceTags = mock[AwsInstanceTags]
+  var devConfig: Configuration = Configuration.from(devConfigValues)
+
+
+  class TestImplementation extends PanDomainAuthActions {
+    override val wsClient: WSClient = mock[WSClient]
+    override val config: AppConfig = new AppConfig(mockTags, devConfig)
+    override val panDomainSettings: PanDomainAuthSettingsRefresher = mock[PanDomainAuthSettingsRefresher]
+    override val controllerComponents: ControllerComponents = mock[ControllerComponents]
+  }
+
+  test("can construct a test implementation") {
+    val instance:TestImplementation = new TestImplementation
+    instance.applicationName must be("foo")
+  }
+}
