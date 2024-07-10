5d4025c0f0b4676211ac59b79c390cfd7a96fe20
David Blatcher <dv.bltchr@gmail.com>
2024-07-10 13:01:23 +0100

    add test for appConfig

--- test/scala/com/gu/viewer/controllers/EmailTest.scala
+++
@@ -1,9 +0,0 @@
-package com.gu.viewer.controllers
-
-import org.scalatest.featurespec.AnyFeatureSpec
-import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.must.Matchers
-
-class EmailTest extends AnyFunSuite with Matchers {
-
-}
