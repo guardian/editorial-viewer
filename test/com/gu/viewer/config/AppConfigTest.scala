9a7a3b7e5df9d8d5388872182edfc6d71c88effa
David Blatcher <dv.bltchr@gmail.com>
2024-07-11 14:16:31 +0100

    test derived values work for PROD config

--- test/com/gu/viewer/config/AppConfigTest.scala
+++ test/com/gu/viewer/config/AppConfigTest.scala
@@ -15,21 +15,40 @@ class AppConfigTest extends AnyFunSuite with Matchers with MockitoSugar {
     "composerReturnUri.DEV" -> "bar"
   )
 
+  val prodConfigValues: Map[String, Any] = Map.apply(
+    "Stage" -> "PROD",
+    "previewHost.DEV" -> "bar",
+    "liveHost.DEV" -> "bar",
+    "composerReturnUri.DEV" -> "bar",
+    "previewHost.PROD" -> "bar",
+    "liveHost.PROD" -> "bar",
+    "composerReturnUri.PROD" -> "bar"
+  )
 
+  def mockTags(configValues: Map[String, Any]): AwsInstanceTags = {
+    val mockTags: AwsInstanceTags = mock[AwsInstanceTags]
+    when(mockTags.readTag("Stage")).thenReturn(configValues.get("Stage") match {
+      case Some(value: String) => Some(value)
+      case _ => None
+    })
+    when(mockTags.readTag("App")).thenReturn(None)
+    mockTags
+  }
 
   test ("can construct with DEV stage") {
-
-    var mockTags: AwsInstanceTags = mock[AwsInstanceTags]
-    var devConfig: Configuration = Configuration.from(devConfigValues)
-
-    when(mockTags.readTag("Stage")).thenReturn(None)
-    when(mockTags.readTag("App")).thenReturn(None)
-
-    val instance: AppConfig = new AppConfig(mockTags,devConfig)
-
+    val instance: AppConfig = new AppConfig(mockTags(devConfigValues),Configuration.from(devConfigValues))
     instance.app must be ("viewer")
     instance.pandaDomain must be ("local.dev-gutools.co.uk")
+    instance.pandaSettingsFileKey must be("local.dev-gutools.co.uk.settings")
     instance.pandaAuthCallback must be ("https://viewer.local.dev-gutools.co.uk/oauthCallback")
+  }
+
+  test("can construct with PROD stage") {
+    val instance: AppConfig = new AppConfig(mockTags(prodConfigValues), Configuration.from(prodConfigValues))
+    instance.app must be("viewer")
+    instance.pandaDomain must be("gutools.co.uk")
+    instance.pandaSettingsFileKey must be("gutools.co.uk.settings")
+    instance.pandaAuthCallback must be("https://viewer.gutools.co.uk/oauthCallback")
   }
 }
 
