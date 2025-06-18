1bf4437d38ec66555b51179c8cc578532aab28b8
Akinsola Lawanson <akinsola.lawanson@guardian.co.uk>
2025-06-18 10:03:30 +0100

    Add RequestHeader to LiveProxyRequest

--- app/com/gu/viewer/proxy/ProxyRequest.scala
+++ app/com/gu/viewer/proxy/ProxyRequest.scala
@@ -10,14 +10,14 @@ object ProxyRequest {
     val queryString = if (request.rawQueryString.nonEmpty) s"?${request.rawQueryString}" else ""
 
     service match {
-      case "live" => LiveProxyRequest(servicePath + queryString, body)
+      case "live" => LiveProxyRequest(servicePath + queryString, request, body)
       case "preview" => PreviewProxyRequest(servicePath + queryString, request, body)
       case _ => UnknownProxyRequest
     }
   }
 }
 
-case class LiveProxyRequest(servicePath: String, body: Option[Map[String, Seq[String]]] = None) extends ProxyRequest
+case class LiveProxyRequest(servicePath: String, request: RequestHeader, body: Option[Map[String, Seq[String]]] = None) extends ProxyRequest
 
 case class PreviewProxyRequest(
   servicePath: String,
