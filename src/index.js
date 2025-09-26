76335608bcc499957d74e56dc1899bb12ab17cdf
Liam Duffy <3338808+liamness@users.noreply.github.com>
2025-09-26 17:01:53 +0100

    WIP preview of both mobile and desktop

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import * as applicationCtrl from "./modules/controllers/application";
 import * as historyCtrl from "./modules/controllers/history";
 import * as keyboardController from "./modules/controllers/keyboard";
+import * as scrollController from "./modules/controllers/scroll"
 
 //Initialize Controllers
 applicationCtrl.init();
 historyCtrl.init();
 keyboardController.init();
+scrollController.init();
