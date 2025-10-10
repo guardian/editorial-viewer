2dfbfc5032794400316e99d7203d0306ea607527
Liam Duffy <3338808+liamness@users.noreply.github.com>
2025-10-10 15:34:18 +0100

    Working POC

--- src/modules/controllers/scroll.js
+++ src/modules/controllers/scroll.js
@@ -1,32 +1,45 @@
+let viewers = [];
+let activeViewer = null;
 
+function addScrollListener(viewer) {
+    viewer.contentWindow.addEventListener('scroll', () => {
+        if (viewer !== activeViewer) {
+            return;
+        }
 
-async function init() {
-    await new Promise(resolve => setTimeout(() => resolve(), 100));
-
-    const viewers = Array.from(document.getElementsByClassName('viewer'));
-    viewers.forEach((viewer, viewerIndex) => {
-        console.log('Adding event listener', viewer, viewer.src, viewer.contentWindow, viewerIndex);
-
-        viewer.contentWindow.addEventListener('scroll', () => {
-            console.log('scrollin')
-            requestAnimationFrame(() => {
-                const { documentElement } = viewer.contentDocument;
-                const scrollTopMax = documentElement.scrollHeight - viewer.contentWindow.innerHeight;
-                const scrollPosition = documentElement.scrollTop / scrollTopMax;
-                const otherViewers = viewers.filter(v => v !== viewer);
-                otherViewers.forEach((otherViewer) => {
-                    otherViewerDocumentElement = otherViewer.contentDocument.documentElement;
-                    otherViewerScrollTopMax = otherViewerDocumentElement.scrollHeight - otherViewer.contentWindow.innerHeight;
-                    otherViewerDocumentElement.scrollTo(0, otherViewerScrollTopMax * scrollPosition);
-                })
-            })
+        requestAnimationFrame(() => {
+            const { documentElement } = viewer.contentDocument;
+            const scrollTopMax = documentElement.scrollHeight - viewer.contentWindow.innerHeight;
+            const scrollPosition = documentElement.scrollTop / scrollTopMax;
+            const otherViewers = viewers.filter(v => v !== viewer);
+            otherViewers.forEach((otherViewer) => {
+                otherViewerDocumentElement = otherViewer.contentDocument.documentElement;
+                otherViewerScrollTopMax = otherViewerDocumentElement.scrollHeight - otherViewer.contentWindow.innerHeight;
+                otherViewerDocumentElement.scrollTo(0, otherViewerScrollTopMax * scrollPosition);
+            });
         });
-    })
+    });
+};
 
-    // TODO: something something mutation observer
+function init() {
+    viewers = Array.from(document.getElementsByClassName('viewer'));
+    viewers.forEach((viewer) => {
+        addScrollListener(viewer);
+
+        viewer.addEventListener('load', () => {
+            addScrollListener(viewer);
+        });
+
+        viewer.addEventListener('mouseenter', (event) => {
+            activeViewer = viewer;
+        });
+
+        viewer.addEventListener('mouseleave', (event) => {
+            activeViewer = null;
+        });
+    });
 }
 
-
 module.exports = {
-    init
+    init,
 };
